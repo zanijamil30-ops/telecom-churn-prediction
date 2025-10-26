9443f1df547c023f67bb466922d595a0c9c54571
Zainab Jamil <zanijamil30@gmail.com>
2025-10-26 23:31:17 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telecom-churn-prediction',
+    version='1.0.0',
+    author='zanjijamil30-ops',
+    author_email='',
+    description='Machine Learning project for predicting telecom customer churn using ensemble and stacking models.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/zanjijamil30-ops/telecom-churn-prediction',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'xgboost',
+        'scipy',
+        'matplotlib',
+        'ipywidgets'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
 
