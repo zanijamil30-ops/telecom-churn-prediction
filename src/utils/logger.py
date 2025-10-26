322766907449919effbab6c2e78e86a1cb36d903
Zainab Jamil <zanijamil30@gmail.com>
2025-10-26 23:57:31 +0500

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,25 @@
+import logging
+from pathlib import Path
+
+def get_logger(name: str = "churn"):
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    logger.setLevel(logging.INFO)
+
+    Path("logs").mkdir(parents=True, exist_ok=True)
+
+    fmt = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+
+    fh = logging.FileHandler("logs/training_logs.txt")
+    fh.setFormatter(fmt)
+    fh.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(fmt)
+    ch.setLevel(logging.INFO)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
 
